d68481dd11098c981d060c7aa40d64af67dad4ca
Justin Miller <Justin.Miller@drivetime.com>
2019-11-21 15:32:20 -0700

    added benchmark tool

---
+++ perf/Benchmark/Program.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ManualConfig.Create(DefaultConfig.Instance);
+            config.Add(MemoryDiagnoser.Default);
+
+            //  AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+            BenchmarkRunner.Run<StringBenchmark>(
+       config
+                //.With(Job.RyuJitX64)
+              //  .With(Job.Clr)
+                 //      .With(new BenchmarkDotNet.Diagnosers.CompositeDiagnoser())
+                 // .With(new BenchmarkDotNet.Diagnosers.CompositeDiagnoser())
+                 .With(ExecutionValidator.FailOnError)
+                 .With(Job.Default.With(ClrRuntime.Net472))
+             );
+            Console.WriteLine("DONE!!!!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ perf/Benchmark/StringBenchmark.cs
@@ -0,0 +1,57 @@
+﻿
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Mathematics;
+using BenchmarkDotNet.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmark
+{
+    //[ShortRunJob]
+    [RankColumn, HtmlExporter, CsvExporter]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn(NumeralSystem.Arabic)]
+    [RankColumn(NumeralSystem.Stars)]
+    public class StringBenchmark
+    {
+        //private static LiteStringBuilder m_strCustom = new LiteStringBuilder(64);
+        //private static System.Text.StringBuilder m_strBuilder = new System.Text.StringBuilder(64);
+
+        [Benchmark]
+        public  string String_Added()
+        {
+            string str = "PI=" + Math.PI + "_373=" + 373 + true + short.MaxValue;
+            return str.Replace("373", "5428");
+        }
+
+        [Benchmark]
+        public  string String_Concat()
+        {
+            return string.Concat("PI=", Math.PI, "_373=", 373, true, short.MaxValue).Replace("373", "5428");
+        }
+
+        [Benchmark]
+        public  string StringBuilder()
+        {
+            System.Text.StringBuilder m_strBuilder = new System.Text.StringBuilder(64);
+            m_strBuilder.Length = 0;
+            m_strBuilder.Append("PI=").Append(Math.PI).Append("_373=").Append(373).Append(true).Append(short.MaxValue).Replace("373", "5428");
+            return m_strBuilder.ToString();
+        }
+
+        [Benchmark]
+        public  string LiteStringBuilder()
+        {
+            LiteStringBuilder m_strCustom = new LiteStringBuilder(64);
+            m_strCustom.Clear();
+            m_strCustom.Append("PI=").Append(Math.PI).Append("_373=").Append(373).Append(true).Append(short.MaxValue).Replace("373", "5428");
+            return m_strCustom.ToString();
+        }
+    }
+}
+
